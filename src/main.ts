b867a6b9e26d1007f1ad1eab59290acc6112f221
Pablo Miralles <pablo@mowomo.com>
2025-02-02 13:19:31 +0100

    Ejercicio terminado

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,14 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+console.log(
+	`Cada comensal tiene que pagar ${
+		(120 - 18) / 6
+	} €, ¡yo invito a las bebidas!`
+);
+console.log(
+	`Por si alguien tiene dudas, la cuenta es de 120€, contando con 18€ de las bebidas. Lo restamos y 120 - 18 = ${
+		120 - 18
+	}. Eso lo dividimos entre 6, y daría el resultado de arriba. 102 / 6 = ${
+		102 / 6
+	}`
+);
